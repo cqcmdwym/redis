fb525f2030761fbf637461d9e6e6fdfcf36e6f50
cqcmdwym <cqcmdwym@126.com>
2018-01-28 00:45:47 +0800

    sorted set

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Strings from './strings';
 import Hashes from './hashes';
 import Lists from './lists';
 import Sets from './sets';
+import SortedSets from './sortedSets';
 
 const redis = new Redis();
 
@@ -11,3 +12,4 @@ Strings(redis);
 Hashes(redis);
 Lists(redis);
 Sets(redis);
+SortedSets(redis);
