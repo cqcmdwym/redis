e14d44773a0b541ec8448d544018f5557cda809e
cqcmdwym <cqcmdwym@126.com>
2018-01-28 00:30:09 +0800

    Set

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import Redis from 'ioredis';
 import Strings from './strings';
 import Hashes from './hashes';
 import Lists from './lists';
+import Sets from './sets';
 
 const redis = new Redis();
 
 Strings(redis);
 Hashes(redis);
 Lists(redis);
+Sets(redis);
