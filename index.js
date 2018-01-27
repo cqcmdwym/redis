4072eb9562b4dbf416efadc867d2942f23953b04
cqcmdwym <cqcmdwym@126.com>
2018-01-28 00:12:52 +0800

    list

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import Redis from 'ioredis';
 //const Redis = require('ioredis');
 import Strings from './strings';
 import Hashes from './hashes';
+import Lists from './lists';
 
 const redis = new Redis();
 
 Strings(redis);
 Hashes(redis);
+Lists(redis);
